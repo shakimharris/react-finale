d8d049341363f116770e272cef5518de1e8a6cdc
amenraprod <amenraprod@gmail.com>
2019-10-15 19:04:01 -0400

    rm old links and added shopping link

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import ShopPage from './pages/shoppage.component';
 import './App.css';
 
-
-const HomePage = (props) => {
-  console.log(props);
-  return (
-    <div> 
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <h1>HOME PAGE </h1> 
-    </div>
-  );
-};
-
-const TopicList = () => {
-  return (
-    <div>
-      <h1>TOPIC LIST</h1>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE {props.match.params.topicId} </h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/topics' component={TopicList} />
-      <Route path='/topics/:topicId' component={TopicDetail} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   )
 }
