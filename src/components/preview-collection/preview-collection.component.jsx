1cc64e44a095c98e8c86633e7c5af9d7a880919d
amenraprod <amenraprod@gmail.com>
2019-10-15 19:48:53 -0400

    rm uppercase func

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -4,7 +4,7 @@ import './preview-collection.styles.scss';
 const CollectionPreview = ({ title, items }) => (
     
         <div className="collection-preview">
-            <h1>{title.toUppercase()}</h1>
+            <h1>{title}</h1>
             <div className="preview">
             {
            items.map(item => (
