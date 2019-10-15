cc13b83ed2684b34f8f1d8bcde017e6384e2b544
amenraprod <amenraprod@gmail.com>
2019-10-15 18:59:05 -0400

    added data to the shopping page

--- src/pages/shoppage.component.jsx
+++ src/pages/shoppage.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-            collection: [ {
-                
-            }]
+            collection: SHOP_DATA
         }
     }
 }
+
+export default ShopPage;
