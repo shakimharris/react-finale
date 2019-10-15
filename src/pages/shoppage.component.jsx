87757bb5011c4d63ea94a1865ec48784fcb07512
amenraprod <amenraprod@gmail.com>
2019-10-15 19:31:53 -0400

    import keyword from

--- src/pages/shoppage.component.jsx
+++ src/pages/shoppage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
-import '../components/preview-collection/preview-collection.component';
+import CollectionPreview from '../components/preview-collection/preview-collection.component';
 
 class ShopPage extends React.Component {
     constructor(props){
