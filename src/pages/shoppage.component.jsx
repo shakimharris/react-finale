37517b5e7802aa45fead3fe36be2624ec361eb58
amenraprod <amenraprod@gmail.com>
2019-10-15 19:27:43 -0400

    imported preview component

--- src/pages/shoppage.component.jsx
+++ src/pages/shoppage.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
+import '../components/preview-collection/preview-collection.component';
 
 class ShopPage extends React.Component {
     constructor(props){
