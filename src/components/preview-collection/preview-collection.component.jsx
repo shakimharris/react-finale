1a2ded2a75125c341364eb0f0fcdfa469dc8c97a
amenraprod <amenraprod@gmail.com>
2019-10-15 19:18:21 -0400

    setup func component

---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './preview-collection.styles.scss'; 
+
+const CollectionPreview = () => (
+    
+        <div>
+            <h1>Title</h1>
+            <div>
+
+            </div>
+        </div>
+)
+
+export default CollectionPreview;
