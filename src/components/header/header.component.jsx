5dbb1ba430709c006f7e5a43c6290e6b321502ba
amenraprod <amenraprod@gmail.com>
2019-10-15 21:08:55 -0400

    created header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+const Header = () => (
+    <div className='header'>
+
+        <Link to='/shop'>SHOP</Link>
+    </div>
+)
+
+export default Header;
