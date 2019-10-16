f1577d3c8e102c41410c35e22ff7db4786683ce9
amenraprod <amenraprod@gmail.com>
2019-10-15 20:17:09 -0400

    added name

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,8 +4,12 @@ import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl}) => (
     <div className='collection-item'>
-        <div className='image'>
-
-        </div>
+        <div className='image'
+        style={{ backgroundImage: `url(${imageUrl})`}}
+        />
+        <div className='collection-footer'>
+        <span className='name'> { name } </span>
+    
+     </div>
     </div>
 )
