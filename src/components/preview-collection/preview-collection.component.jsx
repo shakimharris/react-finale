848bde0c573e10db05c6c6b43b95b5f6bbf1e281
amenraprod <amenraprod@gmail.com>
2019-10-15 20:43:41 -0400

    spread props

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 import './preview-collection.styles.scss'; 
 
 const CollectionPreview = ({ title, items }) => (
-    
-        <div className="collection-preview">
-            <h1>{title}</h1>
-            <div className="preview">
-            {
-           items
-           .filter((items, index) => index < 4)
-           .map(item => (
-                <div key={item.id}>{item.name}</div>))}
-        
-            </div>
-        </div>
-);
-
-export default CollectionPreview;
+    <div className='collection-preview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+  
+  export default CollectionPreview;
--- src/pages/shoppage.component.jsx
+++ src/pages/shoppage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../components/preview-collection/preview-collection.component';
+
 class ShopPage extends React.Component {
     constructor(props) {
       super(props);
