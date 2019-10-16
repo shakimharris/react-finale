99fe0e5ddb15293c8a9914725d826f9d516fb170
amenraprod <amenraprod@gmail.com>
2019-10-15 21:12:55 -0400

    imported svg

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 const Header = () => (
     <div className='header'>
-
+     <Logo className='logo' />
         <Link to='/shop'>SHOP</Link>
     </div>
 )
