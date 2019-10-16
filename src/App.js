13bb0da1d5df6fb487bbc8b46086dd30d94b7266
amenraprod <amenraprod@gmail.com>
2019-10-15 21:57:49 -0400

    added header to all renders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shoppage.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
       <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   )
 }
