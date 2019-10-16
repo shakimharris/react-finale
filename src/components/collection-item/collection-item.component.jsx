342b447f5d14ca4337efb950c6f21bff1ed16173
amenraprod <amenraprod@gmail.com>
2019-10-15 20:18:16 -0400

    added price

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,8 +8,8 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
         style={{ backgroundImage: `url(${imageUrl})`}}
         />
         <div className='collection-footer'>
-        <span className='name'> { name } </span>
-    
+         <span className='name'> { name } </span>
+          <span className='price'>{ price } </span>
      </div>
     </div>
 )
